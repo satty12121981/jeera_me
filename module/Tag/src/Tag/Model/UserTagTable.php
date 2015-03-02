eebc169147b1bb29a840930fb96a0cb36b6e0881
sathish <s.kumar@y2m.ae>
2015-03-02 11:37:21 +0400

    profile user edit delete interests

--- module/Tag/src/Tag/Model/UserTagTable.php
+++ module/Tag/src/Tag/Model/UserTagTable.php
@@ -107,6 +107,39 @@ class UserTagTable extends AbstractTableGateway
 		$resultSet->initialize($statement->execute());	
 		return $resultSet->toArray(); 
 	}
+	public function getAllUserTagsForAdmin($limit,$offset,$field='user_given_name',$order = 'ASC',$search=''){		 
+       	$select = new Select;
+		$select->from('y2m_user')
+			->columns(array('user_first_name', 'user_last_name','user_given_name','user_id'))
+    		->join('y2m_user_tag', 'y2m_user.user_id = y2m_user_tag.user_tag_user_id', array(),'left')
+			->join('y2m_tag', 'y2m_tag.tag_id = y2m_user_tag.user_tag_tag_id', array('tags'=>new Expression('GROUP_CONCAT(y2m_tag.tag_title)')),'left')			 
+			->group('y2m_user.user_id');
+		if($search!=''){
+			$select->where->like('y2m_user.user_given_name',$search.'%')->or->like('y2m_tag.tag_title',$search.'%');		
+		}
+		$select->limit($limit);
+		$select->offset($offset);
+		$select->order($field.' '.$order);
+		$statement = $this->adapter->createStatement();
+		$select->prepareStatement($this->adapter, $statement);
+		//echo $select->getSqlString();die();
+		$resultSet = new ResultSet();
+		$resultSet->initialize($statement->execute());	
+		return $resultSet;	   
+	}
+	public function fetchAllTagsOfUser($user_id)
+    {
+      	$select = new Select;
+		$select->from('y2m_user_tag')
+    		->join('y2m_tag', 'y2m_tag.tag_id = y2m_user_tag.user_tag_tag_id', array('tag_title','tag_id'))			 
+			->where(array('y2m_user_tag.user_tag_user_id' => $user_id))
+			->order(array('y2m_user_tag.user_tag_id ASC'));		 
+		$statement = $this->adapter->createStatement();
+		$select->prepareStatement($this->adapter, $statement);
+		$resultSet = new ResultSet();
+		$resultSet->initialize($statement->execute());	
+		return $resultSet->buffer();
+    }
 	public function getAllUserTags($user_id){
 		$select = new Select;
 		$select->from('y2m_user_tag')
@@ -125,7 +158,7 @@ class UserTagTable extends AbstractTableGateway
 	}
 	public function deleteAllUserTags($user_id,$tag_list=array()){
 		if(!empty($tag_list)){
-			echo $sql = "DELETE FROM y2m_user_tag WHERE user_tag_user_id = ".$user_id." AND user_tag_tag_id NOT IN (".implode(',',$tag_list).")"; 
+			$sql = "DELETE FROM y2m_user_tag WHERE user_tag_user_id = ".$user_id." AND user_tag_tag_id NOT IN (".implode(',',$tag_list).")"; 
 			$statement = $this->adapter-> query($sql); 
 			$statement -> execute();
 		}else{ 	$this->delete(array('user_tag_user_id' => $user_id)); }		
@@ -192,4 +225,43 @@ class UserTagTable extends AbstractTableGateway
 		$resultSet->initialize($statement->execute());	
 		return $resultSet->toArray(); 
 	}
+	public function getCountOfAllUserTags($search=''){
+		$select = new Select;
+		$select->from('y2m_user')
+			->columns(array(new Expression('COUNT(distinct(y2m_user.user_id)) as tag_count')))
+    		->join('y2m_user_tag', 'y2m_user.user_id = y2m_user_tag.user_tag_user_id', array(),'left')
+			->join('y2m_tag', 'y2m_tag.tag_id = y2m_user_tag.user_tag_tag_id', array('tags'=>new Expression('GROUP_CONCAT(y2m_tag.tag_title)')),'left') ;
+		if($search!=''){
+			$select->where->like('y2m_user.user_given_name',$search.'%')->or->like('y2m_tag.tag_title',$search.'%');		
+		}
+		$statement = $this->adapter->createStatement();
+		$select->prepareStatement($this->adapter, $statement);
+		//echo $select->getSqlString();die();
+		$resultSet = new ResultSet();
+		$resultSet->initialize($statement->execute());	
+		return  $resultSet->current()->tag_count;
+	}
+	public function fetchAllTagsExceptUser($user_id,$limit,$offset){
+		$subselect = new Select;
+		$subselect->from('y2m_user_tag')
+			->columns(array(new Expression('distinct(y2m_user_tag.user_tag_tag_id) as tag_id')))    					 
+			;
+		$subselect->where(array('y2m_user_tag.user_tag_user_id' => $user_id));
+		$select = new Select;
+		$select->from('y2m_tag')
+				->columns(array('tag_id','tag_title'))
+				->where->addPredicate(new predicate('y2m_tag.tag_id NOT IN(?)',array($subselect)));
+		$select->limit($limit);
+		$select->offset($offset);
+		$statement = $this->adapter->createStatement();
+		$select->prepareStatement($this->adapter, $statement);
+		//echo $select->getSqlString();die();
+		$resultSet = new ResultSet();
+		$resultSet->initialize($statement->execute());	
+		return $resultSet;
+	}
+	public function removeUserTag($tag_id,$user_id)
+    {
+       return $this->delete(array('user_tag_user_id' => $user_id,'user_tag_tag_id' =>$tag_id ));
+    }
 }
